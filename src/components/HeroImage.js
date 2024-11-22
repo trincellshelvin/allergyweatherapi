f8b5e850d376c8ce4c920cb0612b464d81f0b457
trincellshelvin <trincellshelvin@gmail.com>
2024-11-22 10:36:09 -0600

    adjusted hero image to adjust for vertical height and fill the page

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -2,13 +2,12 @@ import Image from 'next/image';
 
 const HeroImage = () => {
     return (
-        <div className="hero-image-container p-5">
+        <div className="relative w-full h-96 p-5"> 
             <Image
-                src="/Primar.png" // Ensure this path points to your hero image file
+                src="/Primar.png" 
                 alt="Hero Image"
-                layout="responsive"
-                width={800} // Adjust the width as needed
-                height={400} // Adjust the height as needed
+                layout="fill" 
+                objectFit="cover" 
                 className="hero-image"
             />
         </div>
@@ -16,4 +15,3 @@ const HeroImage = () => {
 };
 
 export default HeroImage;
-

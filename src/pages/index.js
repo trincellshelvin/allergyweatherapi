bbad2dd452dd3e8d5da998a5567177494b47a7fe
trincellshelvin <trincellshelvin@gmail.com>
2024-11-23 00:21:55 -0600

    fix possible issue with dependancies within the index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 // pages/index.js
 import { useEffect } from 'react';
-import { useRouter } from 'next/router'; // Ensure complete import
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
     router.push('/splash');
-  }, []);
+  }, [router]); // Add router as a dependency
 
-  return null; // Keep only one return statement
+  return null;
 }

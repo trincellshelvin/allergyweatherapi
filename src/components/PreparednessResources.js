99e5ba1f9a72caf4dc5aef5e935fa57588738dd1
trincellshelvin <trincellshelvin@gmail.com>
2024-11-23 00:09:04 -0600

    updated preparedness resources with ascii formating for proper grammar

--- src/components/PreparednessResources.js
+++ src/components/PreparednessResources.js
@@ -6,12 +6,12 @@ const PreparednessResources = () => {
             <div className="resource-buttons">
                 <Link href="https://www.healthline.com/health/home-remedies-for-allergies" passHref>
                     <button type="button" className="btn btn-outline-primary">
-                        Natural Health Resources - Healthline
+                        Natural Health Resources &#45; Healthline
                     </button>
                 </Link>
                 <Link href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8532887/" passHref>
                     <button type="button" className="btn btn-outline-primary">
-                        An Introduction to Rhinitis - NIH National Library of Medicine
+                        An Introduction to Rhinitis &#45; NIH National Library of Medicine
                     </button>
                 </Link>
                 <Link href="https://www.medicinenet.com/best_home_remedies_for_upper_respiratory_infection/article.htm" passHref>
@@ -22,9 +22,9 @@ const PreparednessResources = () => {
             </div>
 
             <main className="container">
-                <h4 className="text-left">Have a Place to Go...</h4>
-                <h5 className="text-left">Let's Get Prepared</h5>
-                <h6 className="text-left">Keep an Emergency Kit With....</h6>
+                <h4 className="text-left">Have a Place to Go&period;&period;&period;</h4>
+                <h5 className="text-left">Let&#39;s Get Prepared</h5>
+                <h6 className="text-left">Keep an Emergency Kit With&period;&period;&period;&period;</h6>
                 <h6 className="text-left"><u>Checklist</u></h6>
 
                 <div className="container text-justify">
@@ -36,9 +36,9 @@ const PreparednessResources = () => {
                                 <input type="checkbox" name="disposablegloves" id="disposablegloves" />
                                 <label htmlFor="disposablegloves">Disposable Gloves</label><br />
                                 <input type="checkbox" name="emergencysupplies" id="emergencysupplies" />
-                                <label htmlFor="emergencysupplies">Emergency Supplies*</label><br />
+                                <label htmlFor="emergencysupplies">Emergency Supplies&ast;                                </label><br />
                                 <input type="checkbox" name="food" id="food" />
-                                <label htmlFor="food">Non-Perishable Food</label><br />
+                                <label htmlFor="food">Non&#45;Perishable Food</label><br />
                                 <input type="checkbox" name="cookingsupplies" id="cookingsupplies" />
                                 <label htmlFor="cookingsupplies">Cooking Supplies</label>
                             </form>
@@ -48,12 +48,12 @@ const PreparednessResources = () => {
                                 <input type="checkbox" name="clothes" id="clothes" />
                                 <label htmlFor="clothes">A Change of Clothes</label><br />
                                 <input type="checkbox" name="facemasks" id="facemasks" />
-                                <label htmlFor="facemasks">N-95 or better Face Masks</label><br />
+                                <label htmlFor="facemasks">N&#45;95 or better Face Masks</label><br />
                                 <input type="checkbox" name="emergencycontacts" id="emergencycontacts" />
                                 <label htmlFor="emergencycontacts">Emergency Contacts In a Sealed Plastic Bag</label><br />
                                 <input type="checkbox" name="medications" id="medications" />
                                 <label htmlFor="medications">
-                                    All Medications including Those for Allergies (and any inhalers and/or equipment)
+                                    All Medications including Those for Allergies &lparen;and any inhalers and/or equipment&rparen;
                                 </label><br />
                                 <input type="checkbox" name="listofmeds" id="listofmeds" />
                                 <label htmlFor="listofmeds">
@@ -67,11 +67,11 @@ const PreparednessResources = () => {
                 <section style={{ width: 'fit-content' }}></section>
 
                 <div style={{ padding: '5%', textAlign: 'justify', fontSize: 'x-small' }}>
-                    <p><b>*Examples of Emergency Supplies:</b></p>
+                    <p><b>&ast;Examples of Emergency Supplies&colon;</b></p>
                     <p>
-                        Fire starters, hand sanitizer, water, thermal blankets, rain gear, rope, tarp, permanent markers, road hazard equipment (for vehicles),
-                        flashlights, batteries, device chargers, battery packs (rechargeable), cutting tools (saw and/or knife), trash bags, and any other
-                        supplies that you deem necessary for your area.
+                        Fire starters&comma; hand sanitizer&comma; water&comma; thermal blankets&comma; rain gear&comma; rope&comma; tarp&comma; permanent markers&comma; road hazard equipment &lparen;for vehicles&rparen;&comma;
+                        flashlights&comma; batteries&comma; device chargers&comma; battery packs &lparen;rechargeable&rparen;&comma; cutting tools &lparen;saw and&sol;or knife&rparen;&comma; trash bags&comma; and any other
+                        supplies that you deem necessary for your area&period;
                     </p>
                 </div>
             </main>
